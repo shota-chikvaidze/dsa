765b67a2af7f3b652b0ebb10ac988f6269a326ad
shota chikvaidze <shchikvaidze1@gmail.com>
2026-05-31 23:40:03 +0400

    Solve group anagrams

---
+++ neetcode/array/GroupAnagrams.ts
@@ -0,0 +1,27 @@
+// Group Anagrams
+// https://neetcode.io/problems/anagram-groups
+
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+
+        let map = new Map()
+
+        for(const str of strs) {
+            const key = str.split("").sort().join("")
+            const group = map.get(key)
+            
+            if(group) {
+                group.push(str)
+            }else {
+                map.set(key, [str])
+            }
+        }
+
+        return [...map.values()]
+
+    }
+}
