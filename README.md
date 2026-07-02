57b87688073e6a76f339707f74c42c888b3a3499
shota chikvaidze <shchikvaidze1@gmail.com>
2026-07-02 20:48:57 +0400

    Solve Invert Binary Tree

--- README.md
+++ README.md
@@ -54,3 +54,4 @@ dsa/
 | 2026-06-28 | Remove Nth Node From End of List | medium | Linked List |
 | 2026-06-29 | Find the Duplicate Number | medium | Linked List |
 | 2026-06-30 | Reorder Linked List | medium | Linked List |
+| 2026-06-2 | Invert Binary Tree | easy | Tree |
---
+++ neetcode/tree/InvertBinaryTree.ts
@@ -0,0 +1,36 @@
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+
+    constructor(val = 0, left: TreeNode | null = null, right: TreeNode | null = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root: TreeNode | null): TreeNode | null {
+
+        if(root === null) {
+            return null
+        }
+
+        let tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        
+        this.invertTree(root.left)
+        this.invertTree(root.right)
+
+        return root
+    }
+}
